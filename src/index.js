5b6aa435420bc82bab8f0eaf2679dd01446d791c
Steven Silvestri <steven.m.silvestri@gmail.com>
2017-09-14 17:11:24 -0400

    fix(Fix import name):

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-// module.exports = {
-// 	ModalContainer: require('./container'),
-// 	Actions       : require('./actions').Actions,
-// 	setModal      : require('./actions').setModal,
-// 	unsetModal    : require('./actions').unsetModal,
-// 	reducer       : require('./reducer')
-// };
-import Modalcontainer from './container/ModalContainer';
+import ModalContainer from './container/ModalContainer';
 import { Actions, setModal, unsetModal } from './actions';
 import reducer from './reducer';
 
-export { Modalcontainer, reducer, Actions, setModal, unsetModal };
+export { ModalContainer, reducer, Actions, setModal, unsetModal };
